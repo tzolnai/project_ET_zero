84677a0ab08e91849079cdc8d4c10e9233324549
hallgatoemese <51368252+hallgatoemese@users.noreply.github.com>
2019-06-05 13:29:05 +0200

    Update asrt SzHE 20180404 vP3.py

--- asrt SzHE 20180404 vP3.py
+++ asrt SzHE 20180404 vP3.py
@@ -1,5 +1,23 @@
+
+#    ASRT script in Psychopy
+#    Copyright (C) <2018>  <Emese Szegedi-Hallgató>
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses
+
 ##!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 
 from psychopy import visual, core, event, gui, monitors, sound, prefs
 import shelve, random, codecs, os, time, matplotlib
