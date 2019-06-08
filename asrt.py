6733527ecc567aff32c987211ce012e7c73bc879
Tamás Zolnai <zolnaitamas2000@gmail.com>
2019-06-08 10:00:45 +0200

    It's 2019 actually.

--- asrt.py
+++ asrt.py
@@ -1,7 +1,7 @@
 
 #    ASRT script in Psychopy
 #    Copyright (C) <2018>  <Emese Szegedi-Hallgató>
-#                  <2018>  <Tamás Zolnai>  <zolnaitamas2000@gmail.com>
+#                  <2019>  <Tamás Zolnai>  <zolnaitamas2000@gmail.com>
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
--- test/show_subject_settings_dialog_test.py
+++ test/show_subject_settings_dialog_test.py
@@ -1,4 +1,4 @@
-#    Copyright (C) <2018>  <Tamás Zolnai>  <zolnaitamas2000@gmail.com>
+#    Copyright (C) <2019>  <Tamás Zolnai>  <zolnaitamas2000@gmail.com>
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
