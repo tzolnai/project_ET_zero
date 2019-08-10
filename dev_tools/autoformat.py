ec94645c7add6632042a5d16088d1c66d9cf5ca2
Tamás Zolnai <zolnaitamas2000@gmail.com>
2019-08-10 19:31:29 +0200

    Change max line length to 100 character.
    By default it is 72.

--- dev_tools/autoformat.py
+++ dev_tools/autoformat.py
@@ -6,7 +6,7 @@ def format_files_in_directory(dir_path):
         file_path = os.path.join(dir_path, file)
         if os.path.isfile(file_path) and file_path.endswith(".py"):
             print("Formating file: " + file_path)
-            os.system("autopep8 -i --ignore E402 " + file_path)
+            os.system("autopep8 -i --ignore E402 --max-line-length 100 " + file_path)
         elif os.path.isdir(file_path) and not file_path.endswith("externals"):
             print("Stepping into directory: " + file_path)
             format_files_in_directory(file_path)
