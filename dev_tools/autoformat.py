f1e7e3fd1d89d2edb80d17c1aac2ff9e09606bc9
Tamás Zolnai <zolnaitamas2000@gmail.com>
2019-08-11 10:04:38 +0200

    Use linux like line endings in all script files
    So it will be consistent across all the files.

--- dev_tools/autoformat.py
+++ dev_tools/autoformat.py
@@ -6,7 +6,18 @@ def format_files_in_directory(dir_path):
         file_path = os.path.join(dir_path, file)
         if os.path.isfile(file_path) and file_path.endswith(".py"):
             print("Formating file: " + file_path)
+
+            # Run autopep8
             os.system("autopep8 -i --ignore E402 --max-line-length 100 " + file_path)
+
+            # Replace line endings (use Linux line endings consitently)
+            with open(file_path, 'rb') as open_file:
+                content = open_file.read()
+            if b'\r\n' in content:
+                content = content.replace(b'\r\n', b'\n')
+                with open(file_path, 'wb') as open_file:
+                    open_file.write(content)
+
         elif os.path.isdir(file_path) and not file_path.endswith("externals"):
             print("Stepping into directory: " + file_path)
             format_files_in_directory(file_path)
