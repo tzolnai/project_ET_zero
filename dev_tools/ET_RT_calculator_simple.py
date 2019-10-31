a1e56b8a718a362f668f2c4507d2822aa67aa1bb
Tamás Zolnai <zolnaitamas2000@gmail.com>
2019-10-31 23:27:05 +0100

    ET_RT_calculator: Don't use hard coded indices

--- dev_tools/ET_RT_calculator_simple.py
+++ dev_tools/ET_RT_calculator_simple.py
@@ -33,23 +33,26 @@ def convert(raw_file_name, new_file_name):
     new_file_data.write('RT (samples)')
     new_file_data.write('\n')
 
+    trial_pos = raw_lines[0].split('\t').index("trial")
+    RSI_pos = raw_lines[0].split('\t').index("RSI_time")
+
     last_trial = "1"
-    counter = 0
+    RT_counter = 0
     current_line = 1
     for line in raw_lines[1:]:
-        if last_trial != line.split('\t')[13] or line == raw_lines[len(raw_lines) - 1]:
+        if last_trial != line.split('\t')[trial_pos] or line == raw_lines[len(raw_lines) - 1]:
             new_file_data.write((raw_lines[current_line - 1])[:len(raw_lines[current_line - 1]) - 1])
             new_file_data.write('\t')
             if line == raw_lines[len(raw_lines) - 1]:
-                new_file_data.write(str(counter + 1))
+                new_file_data.write(str(RT_counter + 1))
             else:
-                new_file_data.write(str(counter))
+                new_file_data.write(str(RT_counter))
             new_file_data.write('\n')
-            last_trial = line.split('\t')[13]
-            counter = 0
+            last_trial = line.split('\t')[trial_pos]
+            RT_counter = 0
         current_line += 1
-        if line.split('\t')[14] != "-1":
-            counter += 1
+        if line.split('\t')[RSI_pos] != "-1":
+            RT_counter += 1
 
     with codecs.open(new_file_name, 'w', encoding='utf-8') as new_output_file:
         new_output_file.write(new_file_data.getvalue())
