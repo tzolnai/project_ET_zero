e1f0916391f582efab81e7e9372cd719ce52f10c
Tamás Zolnai <zolnaitamas2000@gmail.com>
2019-12-25 20:24:54 +0100

    Add testMoreSessions to ET integration test suit.

--- test/integration_ET_test.py
+++ test/integration_ET_test.py
@@ -349,5 +349,16 @@ class integrationTest(unittest.TestCase):
 
         self.checkOutputFile()
 
+    def testMoreSessions(self):
+        gui_mock = pgm.PsychoPyGuiMock()
+        gui_mock.addFieldValues(
+            ['Tóth Béla', 10, 'férfi', 25, '3rd', '3rd', '3rd'])
+
+        self.visual_mock = pvm.PsychoPyVisualMock()
+
+        self.experiment.run(window_gammaErrorPolicy='ignore')
+
+        self.checkOutputFile()
+
 if __name__ == "__main__":
     unittest.main()  # run all tests
