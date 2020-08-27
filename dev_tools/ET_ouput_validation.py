439772790ecb4746c7340af5dbf965ca8a519127
Tamás Zolnai <zolnaitamas2000@gmail.com>
2020-08-27 16:32:06 +0200

    Update output validator.

--- dev_tools/ET_ouput_validation.py
+++ dev_tools/ET_ouput_validation.py
@@ -32,10 +32,28 @@ class OutputValidation(unittest.TestCase):
 
         column = data_table["computer_name"]
         self.assertTrue(len(column) > 0)
-        computer_name = column[0]
-        self.assertEqual(computer_name, "Laposka")
         for cell in column:
-            self.assertEqual(cell, computer_name)
+            self.assertEqual(cell, "ET-PC")
+
+        print("OK")
+
+    def validate_monitor_width(self, data_table):
+        print("Validate monitor width...")
+
+        column = data_table["monitor_width_pixel"]
+        self.assertTrue(len(column) > 0)
+        for cell in column:
+            self.assertEqual(cell, 1920)
+
+        print("OK")
+
+    def validate_monitor_height(self, data_table):
+        print("Validate monitor height...")
+
+        column = data_table["monitor_height_pixel"]
+        self.assertTrue(len(column) > 0)
+        for cell in column:
+            self.assertEqual(cell, 1080)
 
         print("OK")
 
@@ -48,17 +66,6 @@ class OutputValidation(unittest.TestCase):
         self.assertTrue(math.isnan(subject_group))
         for cell in column:
             self.assertTrue(math.isnan(cell))
-
-        print("OK")
-
-    def validate_subject_name(self, data_table):
-        print("Validate subject name...")
-
-        column = data_table["subject_name"]
-        self.assertTrue(len(column) > 0)
-        subject_name = column[0]
-        for cell in column:
-            self.assertEqual(cell, subject_name)
 
         print("OK")
 
@@ -107,42 +114,59 @@ class OutputValidation(unittest.TestCase):
     def validate_asrt_type(self, data_table):
         print("Validate asrt type...")
 
-        column = data_table["asrt_type"]
-        self.assertTrue(len(column) > 0)
+        asrt_type_column = data_table["asrt_type"]
+        epoch_column = data_table["epoch"]
+        self.assertTrue(len(asrt_type_column) > 0)
 
-        asrt_type = column[0]
-        self.assertTrue(asrt_type in ["implicit", "explicit", "noASRT"])
-
-        for cell in column:
-            self.assertEqual(cell, asrt_type)
+        for row in range(len(asrt_type_column)):
+            if epoch_column[row] == 1:
+                self.assertEqual(asrt_type_column[row], "noASRT")
+            else:
+                self.assertEqual(asrt_type_column[row], "implicit")
 
         print("OK")
 
     def validate_PCode(self, data_table):
         print("Validate PCode...")
 
-        column = data_table["PCode"]
-        self.assertTrue(len(column) > 0)
+        PCode_column = data_table["PCode"]
+        epoch_column = data_table["epoch"]
+        self.assertTrue(len(PCode_column) > 0)
 
-        PCode = column[0]
-        self.assertTrue(str(PCode) in ["1234", "1243", "1324", "1342", "1423", "1432", "noPattern"])
-
-        for cell in column:
-            self.assertEqual(cell, PCode)
+        first_row_of_8th_epoch = -1
+        first_row_of_2nd_epoch = -1
+        for row in range(len(PCode_column)):
+            self.assertTrue(str(PCode_column[row]) in ["1234", "1243", "1324", "1342", "1423", "1432", "noPattern"])
+            if epoch_column[row] == 1:
+                self.assertEqual(PCode_column[row], "noPattern")
+            elif epoch_column[row] in [2, 3, 4, 5, 6, 8]:
+                if first_row_of_2nd_epoch == -1:
+                    first_row_of_2nd_epoch = row
+                self.assertEqual(str(PCode_column[row]), str(PCode_column[first_row_of_2nd_epoch]))
+            elif epoch_column[row] == 8:
+                if first_row_of_8th_epoch == -1:
+                    first_row_of_8th_epoch = row
+                self.assertEqual(str(PCode_column[row]), str(PCode_column[first_row_of_8th_epoch]))
 
         print("OK")
 
     def validate_session(self, data_table):
         print("Validate session...")
 
-        column = data_table["session"]
-        self.assertTrue(len(column) > 0)
+        session_column = data_table["session"]
+        epoch_column = data_table["epoch"]
+        self.assertTrue(len(session_column) > 0)
 
-        session = column[0]
-        self.assertTrue(int(session) >= 1)
 
-        for cell in column:
-            self.assertEqual(cell, session)
+        for row in range(len(session_column)):
+            if epoch_column[row] in [1, 2, 3, 4, 5]:
+                self.assertEqual(session_column[row], 1)
+            else:
+                self.assertEqual(session_column[row], 2)
+
+            if row != 0:
+                self.assertTrue(session_column[row] == session_column[row - 1] or
+                                session_column[row] == session_column[row - 1] + 1)
 
         print("OK")
 
@@ -154,10 +178,18 @@ class OutputValidation(unittest.TestCase):
         self.assertTrue(len(epoch_column) > 0)
         self.assertEqual(len(epoch_column), len(block_column))
 
-        for i in range(len(epoch_column)):
-            block_number = block_column[i]
-            self.assertEqual(epoch_column[i], (block_number - 1) // 5 + 1)
-            self.assertTrue(epoch_column[i] >= 1 and epoch_column[i] <= 5)
+        for row in range(len(epoch_column)):
+            block_number = block_column[row]
+            epoch_number = epoch_column[row]
+            if block_number == 0:
+                self.assertTrue(epoch_number == 1 or epoch_number == 6)
+            else:                
+                self.assertEqual(epoch_number, (block_number - 1) // 5 + 1)
+            self.assertTrue(epoch_number >= 1 and epoch_number <= 8)
+            
+            if row != 0:
+                self.assertTrue(epoch_number == epoch_column[row - 1] or
+                                epoch_number == epoch_column[row - 1] + 1)
 
         print("OK")
 
@@ -169,12 +201,21 @@ class OutputValidation(unittest.TestCase):
         self.assertTrue(len(block_column) > 0)
         self.assertEqual(len(block_column), len(trial_column))
 
-        expected_block_number = 1
-        for i in range(len(block_column)):
-            if i > 0 and trial_column[i] == 1 and trial_column[i - 1] != 1:
+        expected_block_number = 0
+        for row in range(len(block_column)):
+            block_number = block_column[row]
+            if row > 0 and trial_column[row] == 1 and trial_column[row - 1] != 1 and block_number != 0:
                 expected_block_number += 1
-            self.assertEqual(block_column[i], expected_block_number)
-            self.assertTrue(block_column[i] >= 1 and block_column[i] <= 25)
+            
+            if block_number != 0:
+                self.assertEqual(block_number, expected_block_number)
+            else:
+                self.assertTrue(trial_column[row] <= 20)
+            self.assertTrue(block_number >= 0 and block_number <= 40)
+
+            if row != 0 and block_number != 0 and block_column[row - 1] != 0:
+                self.assertTrue(block_number == block_column[row - 1] or
+                                block_number == block_column[row - 1] + 1)
 
         print("OK")
 
@@ -182,28 +223,39 @@ class OutputValidation(unittest.TestCase):
         print("Validate trial...")
 
         trial_column = data_table["trial"]
+        block_column = data_table["block"]
         RSI_column = data_table["RSI_time"]
         self.assertTrue(len(trial_column) > 0)
         self.assertEqual(len(trial_column), len(RSI_column))
 
         expected_trial_number = 1
         trial_displayed = False
-        for i in range(len(trial_column)):
-            if i > 0 and RSI_column[i] == "-1" and RSI_column[i - 1] != "-1":
+        for row in range(len(trial_column)):
+            trial_num = trial_column[row]
+            if row > 0 and RSI_column[row] == "-1" and RSI_column[row - 1] != "-1":
                 expected_trial_number += 1
                 trial_displayed = False
 
-            if RSI_column[i] != "-1":
+            if RSI_column[row] != "-1":
                 trial_displayed = True
 
-            if trial_column[i] != expected_trial_number and not trial_displayed:
+            if trial_num != expected_trial_number and not trial_displayed:
                 expected_trial_number += 1
 
-            if expected_trial_number > 80:
+            if (row != 0 and block_column[row - 1] == 0 and expected_trial_number > 20) or expected_trial_number > 82:
                 expected_trial_number = 1
 
-            self.assertEqual(trial_column[i], expected_trial_number)
-            self.assertTrue(trial_column[i] >= 1 and trial_column[i] <= 80)
+            self.assertEqual(trial_num, expected_trial_number)
+
+            if block_column[row] == 0:
+                self.assertTrue(trial_num >= 1 and trial_num <= 20)
+            else:
+                self.assertTrue(trial_num >= 1 and trial_num <= 82)
+                
+            if row != 0:
+                self.assertTrue(trial_num == trial_column[row - 1] or
+                                trial_num == trial_column[row - 1] + 1 or
+                                trial_num == 1)
 
         print("OK")
 
@@ -217,13 +269,13 @@ class OutputValidation(unittest.TestCase):
         self.assertEqual(len(RSI_column), len(trial_phase_column))
 
         expected_block_number = 1
-        for i in range(len(RSI_column)):
-            if trial_phase_column[i] == "before_stimulus":
-                self.assertEqual(RSI_column[i], "-1")
-            elif trial_column[i] == 1:
-                self.assertEqual(RSI_column[i], "0,0")
+        for row in range(len(RSI_column)):
+            if trial_phase_column[row] == "before_stimulus":
+                self.assertEqual(RSI_column[row], "-1")
+            elif trial_column[row] == 1:
+                self.assertEqual(RSI_column[row], "0,0")
             else:
-                self.assertTrue(abs(float(RSI_column[i].replace(',', '.')) - 0.500) < 0.03)
+                self.assertTrue(abs(float(RSI_column[row].replace(',', '.')) - 0.500) < 0.03)
 
         print("OK")
 
@@ -233,11 +285,8 @@ class OutputValidation(unittest.TestCase):
         column = data_table["frame_rate"]
         self.assertTrue(len(column) > 0)
 
-        frame_rate = column[0]
-        self.assertTrue(abs(float(frame_rate.replace(',', '.')) - 60.0) < 0.5)
-
         for cell in column:
-            self.assertEqual(cell, frame_rate)
+            self.assertTrue(abs(float(cell.replace(',', '.')) - 60.0) < 0.5)
 
         print("OK")
 
@@ -247,14 +296,10 @@ class OutputValidation(unittest.TestCase):
         column = data_table["frame_time"]
         self.assertTrue(len(column) > 0)
 
-        frame_time = column[0]
-        self.assertTrue(abs(float(frame_time.replace(',', '.')) - (1000.0 / 60.0)) < 0.05)
-
-        frame_rate = float(data_table["frame_rate"][0].replace(',', '.'))
-        self.assertTrue(abs(float(frame_time.replace(',', '.')) - (1000.0 / frame_rate)) < 0.06)
-
-        for cell in column:
-            self.assertEqual(cell, frame_time)
+        for row in range(len(column)):
+            frame_rate = float(data_table["frame_rate"][row].replace(',', '.'))
+            self.assertTrue(abs(float(column[row].replace(',', '.')) - (1000.0 / 60.0)) < 0.05)
+            self.assertTrue(abs(float(column[row].replace(',', '.')) - (1000.0 / frame_rate)) < 0.08)
 
         print("OK")
 
@@ -264,11 +309,9 @@ class OutputValidation(unittest.TestCase):
         column = data_table["frame_sd"]
         self.assertTrue(len(column) > 0)
 
-        frame_sd = column[0]
-        self.assertTrue(abs(float(frame_sd.replace(',', '.'))) < 0.1)
-
         for cell in column:
-            self.assertEqual(cell, frame_sd)
+            print(cell)
+            self.assertTrue(abs(float(cell.replace(',', '.'))) < 0.5)
 
         print("OK")
 
@@ -278,65 +321,70 @@ class OutputValidation(unittest.TestCase):
         column = data_table["stimulus_color"]
         self.assertTrue(len(column) > 0)
 
-        stimulus_color = column[0]
-        self.assertEqual(stimulus_color, "DarkBlue")
-
         for cell in column:
-            self.assertEqual(cell, stimulus_color)
+            self.assertEqual(cell, "DarkBlue")
 
         print("OK")
 
-    def validate_pattern_random_and_trial_relation(self, data_table):
+    def validate_trial_type_pr(self, data_table):
         print("Validate pattern/random...")
 
-        pr_column = data_table["pattern_or_random"]
+        pr_column = data_table["trial_type_pr"]
         trial_column = data_table["trial"]
+        block_column = data_table["block"]
+        epoch_column = data_table["epoch"]
         self.assertTrue(len(pr_column) > 0)
         self.assertEqual(len(pr_column), len(trial_column))
 
-        for i in range(len(pr_column)):
-            if trial_column[i] % 2 == 1:
-                self.assertEqual(pr_column[i], "pattern")
+        for row in range(len(pr_column)):
+            if str(block_column[row]) == "0" or epoch_column[row] == 1 or trial_column[row] <= 2:
+                self.assertEqual(pr_column[row], "random")
+            elif trial_column[row] % 2 == 1:
+                self.assertEqual(pr_column[row], "pattern")
             else:
-                self.assertEqual(pr_column[i], "random")
+                self.assertEqual(pr_column[row], "random")
 
         print("OK")
 
     def validate_triplet_type_hl(self, data_table):
         print("Validate triplet frequency...")
 
-        high_low_column = data_table["triplet_frequency"]
+        high_low_column = data_table["triplet_type_hl"]
         trial_column = data_table["trial"]
-        pr_column = data_table["pattern_or_random"]
+        pr_column = data_table["trial_type_pr"]
         stimulus_column = data_table["stimulus"]
+        block_column = data_table["block"]
         self.assertTrue(len(high_low_column) > 0)
         self.assertEqual(len(high_low_column), len(trial_column))
 
-        PCode = str(data_table["PCode"][0])
-        dict_next_stimulus = {}
-        dict_next_stimulus[PCode[0]] = PCode[1]
-        dict_next_stimulus[PCode[1]] = PCode[2]
-        dict_next_stimulus[PCode[2]] = PCode[3]
-        dict_next_stimulus[PCode[3]] = PCode[0]
-
         last_random_stimulus = 0
-        for i in range(len(high_low_column)):
-            if trial_column[i] <= 2:
-                #self.assertEqual(high_low_column[i], "none")
+        for row in range(len(high_low_column)):
+            if str(block_column[row]) == "0":
                 pass
-            elif pr_column[i] == "pattern":
-                self.assertEqual(high_low_column[i], "high")
+            elif trial_column[row] <= 2:
+                self.assertEqual(high_low_column[row], "none")
+            elif pr_column[row] == "pattern":
+                self.assertEqual(high_low_column[row], "high")
             else:
-                if trial_column[i] != trial_column[i - 1]:
-                    if str(stimulus_column[i]) == dict_next_stimulus[str(last_random_stimulus)]:
-                        self.assertEqual(high_low_column[i], "high")
-                    else:
-                        self.assertEqual(high_low_column[i], "low")
-                else:
-                    self.assertEqual(high_low_column[i], high_low_column[i - 1])
+                PCode = str(data_table["PCode"][row])
+                dict_next_stimulus = {}
+                dict_next_stimulus[PCode[0]] = PCode[1]
+                dict_next_stimulus[PCode[1]] = PCode[2]
+                dict_next_stimulus[PCode[2]] = PCode[3]
+                dict_next_stimulus[PCode[3]] = PCode[0]
 
-            if pr_column[i] == "random":
-                last_random_stimulus = stimulus_column[i]
+                if PCode == "noPattern":
+                    self.assertEqual(high_low_column[row], "none")
+                elif trial_column[row] != trial_column[row - 1]:
+                    if str(stimulus_column[row]) == dict_next_stimulus[str(last_random_stimulus)]:
+                        self.assertEqual(high_low_column[row], "high")
+                    else:
+                        self.assertEqual(high_low_column[row], "low")
+                else:
+                    self.assertEqual(high_low_column[row], high_low_column[row - 1])
+
+            if pr_column[row] == "random":
+                last_random_stimulus = stimulus_column[row]
 
         print("OK")
 
@@ -358,12 +406,12 @@ class OutputValidation(unittest.TestCase):
         dict_next_stimulus[PCode[3]] = PCode[0]
 
         last_pattern_stimulus = 0
-        for i in range(len(stimulus_column)):
-            self.assertTrue(stimulus_column[i] in [1, 2, 3, 4])
-            if pr_column[i] == "pattern":
-                if trial_column[i] > 1 and stimulus_column[i] != last_pattern_stimulus:
-                    self.assertEqual(str(stimulus_column[i]), dict_next_stimulus[str(last_pattern_stimulus)])
-                last_pattern_stimulus = stimulus_column[i]
+        for row in range(len(stimulus_column)):
+            self.assertTrue(stimulus_column[row] in [1, 2, 3, 4])
+            if pr_column[row] == "pattern":
+                if trial_column[row] > 1 and stimulus_column[row] != last_pattern_stimulus:
+                    self.assertEqual(str(stimulus_column[row]), dict_next_stimulus[str(last_pattern_stimulus)])
+                last_pattern_stimulus = stimulus_column[row]
 
         print("OK")
 
@@ -656,23 +704,25 @@ class OutputValidation(unittest.TestCase):
     def validate(self, file_name):
         data_table = pandas.read_csv(file_name, sep='\t')
 
-        self.validate_computer_name(data_table)
-        self.validate_subject_group(data_table)
-        self.validate_subject_name(data_table)
-        self.validate_subject_number(data_table)
-        self.validate_subject_sex(data_table)
-        self.validate_subject_age(data_table)
-        self.validate_asrt_type(data_table)
-        self.validate_PCode(data_table)
-        self.validate_session(data_table)
-        self.validate_epoch_block_relation(data_table)
-        self.validate_block_trial_relation(data_table)
-        self.validate_trial_RSI_relation(data_table)
-        self.validate_RSI_trial_phase_relation(data_table)
-        self.validate_frame_rate(data_table)
-        self.validate_frame_time(data_table)
-        self.validate_frame_sd(data_table)
-        self.validate_pattern_random_and_trial_relation(data_table)
+        #self.validate_computer_name(data_table)
+        #self.validate_monitor_width(data_table)
+        #self.validate_monitor_height(data_table)
+        #self.validate_subject_group(data_table)
+        #self.validate_subject_number(data_table)
+        #self.validate_subject_sex(data_table)
+        #self.validate_subject_age(data_table)
+        #self.validate_asrt_type(data_table)
+        #self.validate_PCode(data_table)
+        #self.validate_session(data_table)
+        #self.validate_epoch_block_relation(data_table)
+        #self.validate_block_trial_relation(data_table)
+        #self.validate_trial_RSI_relation(data_table)
+        #self.validate_RSI_trial_phase_relation(data_table)
+        #self.validate_frame_rate(data_table)
+        #self.validate_frame_time(data_table)
+        #self.validate_frame_sd(data_table)
+        #self.validate_stimulus_color(data_table)
+        #self.validate_trial_type_pr(data_table)
         self.validate_triplet_type_hl(data_table)
         self.validate_stimulus(data_table)
         # self.validate_RSI_interval_sampling(data_table)
