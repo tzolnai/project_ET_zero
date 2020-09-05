d68c3d690d36361fd76559fb981b7b0964ee086b
Tamás Zolnai <zolnaitamas2000@gmail.com>
2020-09-05 21:06:11 +0200

    Be precise here.
    We allow 1/3 of the data to be missing.

--- asrt.py
+++ asrt.py
@@ -1746,11 +1746,11 @@ class Experiment:
                     if count >= fixation_threshold:
                         break
 
-                    if invalid_count > fixation_threshold * 0.35:
+                    if invalid_count > fixation_threshold * 0.334:
                         break
 
-                # We have too many invalid data (we allow maximum 35% to be invalid)
-                if invalid_count > fixation_threshold * 0.35:
+                # We have too many invalid data (we allow maximum 33.3333% to be invalid)
+                if invalid_count > fixation_threshold * 0.334:
                     continue
 
                 # Do we have engough data for a fixation?
